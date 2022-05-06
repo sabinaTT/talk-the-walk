eec3be29e2e3057e1794dbaa736d2160c1e31fe2
sabinaTT <tairsbn@gmail.com>
2022-05-05 22:52:21 -0700

    create paths for home/about and their respective view functions

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+class Home(View):
+    def get(self, request):
+        return HttpResponse('All great stories begin in two ways..')
+
+class About(View):
+    def get(self, request):
+        return HttpResponse('ABOUT time to start!')
