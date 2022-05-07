8eb710f32608dbc11dc2e8dafe176339bacf213c
sabinaTT <tairsbn@gmail.com>
2022-05-07 10:13:07 -0700

    render data in a template

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('about/', views.About.as_view(), name='about'),
-    
+    path('stacks/', views.ListStacks.as_view(), name='list-stacks')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,20 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = 'about.html'
+
+class Stack:
+    def __init__(self, name):
+        self.name = name
+
+stacks = [
+    Stack("Frontend"),
+    Stack("Backend")
+]
+
+class ListStacks(TemplateView):
+    template_name = 'list_stacks.html'
+    # kwargs allow you to pass keyword arguments to a function
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['stacks'] = stacks
+        return context
