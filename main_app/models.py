312a072984a005b80f3bafb3b176c3feaa3e1934
sabinaTT <tairsbn@gmail.com>
2022-05-07 11:56:38 -0700

    create stack model and run migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Stack(models.Model):
+    name = models.CharField(max_length=70)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
