b63936a4c2f62c4b756418523d83e61e7308954c
sabinaTT <tairsbn@gmail.com>
2022-05-11 11:49:57 -0700

    create urls for Question Model CRUD

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('stacks/<int:pk>/', views.StackDetail.as_view(), name='stack_detail'),
     path('stacks/<int:pk>/update', views.Update_Stack.as_view(), name='update-stack'),
     # path('stacks/<int:pk>delete', views.DeleteStack.as_view(), name='delete-stack'),
+    path('questions/', views.Questions_index, name='list_questions'),
+    path('questions/<int:question_id>', views.View_Question, name='view_question'),
+    path('questions/create/', views.Create_Question.as_view(), name='create_question'),
+    path='questions/<int:pk>/update/', views.Update_Question.as_view(), name='update_question'),
+    path='questions/<int:pk>/delete/', views.Delete_Question.as_view(), name='Delete_Question'),
 ]
