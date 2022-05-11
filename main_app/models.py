87e23aa8b5a718ec1e7fc0c4f90f72f40693a18b
sabinaTT <tairsbn@gmail.com>
2022-05-11 11:19:39 -0700

    create one to many relationships between User & Stack, Stack & Question

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Stack(models.Model):
     name = models.CharField(max_length=70)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -16,6 +18,7 @@ class Question(models.Model):
     the_question = models.CharField(max_length=100)
     answer = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
+    stack = models.ForeignKey(Stack, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.the_question
