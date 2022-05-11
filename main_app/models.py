90a1526f4eb25627b3f93574320c820ebbeec5d7
sabinaTT <tairsbn@gmail.com>
2022-05-11 10:55:48 -0700

    create a question model

--- main_app/models.py
+++ main_app/models.py
@@ -10,3 +10,20 @@ class Stack(models.Model):
     
     class Meta:
         ordering = ['name']
+
+
+class Question(models.Model):
+    the_question = models.CharField(max_length=100)
+    answer = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.the_question
+
+    class Meta:
+        ordering = ['created_at']
+
+
+
+
+
