2d1d408f189c4fc7897a30f3986cc3fa20ee167d
sabinaTT <tairsbn@gmail.com>
2022-05-11 23:40:05 -0700

    style home page

--- main_app/models.py
+++ main_app/models.py
@@ -2,25 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Question(models.Model):
-    the_question = models.CharField(max_length=100)
-    answer = models.TextField(max_length=1000)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.the_question
-
 class Stack(models.Model):
     name = models.CharField(max_length=70)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.name
     
     class Meta:
         ordering = ['name']
+
+class Question(models.Model):
+    the_question = models.CharField(max_length=100)
+    answer = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    stack = models.ForeignKey(Stack, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.the_question
 
 
 
